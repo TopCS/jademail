d4aa39ebf0fb16f50f5b55dd5fe4af2cbaaa1116
Valerio Coltrè <mister.gamer@gmail.com>
2012-12-21 17:43:29 +0100

    First working release! :) Documentation ongoing, Tests TBD!  On branch master  Your branch is ahead of 'origin/master' by 1 commit. 	modified:   README.md 	modified:   index.js 	modified:   lib/jademail.js 	modified:   package.json 	modified:   test/test.js

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-jademail
-========
+###Jademail
+###========
 
 Simple jade email sender, with caching.
+
+##How to include
+>Jademail requires an argument when require()d, this path is the email profiles folder
+```javascript
+var jademail = require('jademail')('./folder/emails');
+```
+>It automatically loads (and cache) all the profiles found in the subdirs, when done it calls the event 'profilesLoaded'
+```javascript
+jademail.on('profilesLoaded', function (path) {
+  console.log('Profile Path: %s loaded', path);
+});
+```
+
+###Usage Example
+See [examples](./jademail/tree/master/examples)
+
+###Methods
+>TODO!
+
+###Events
+>LAZY!
--- test/test.js
+++ test/test.js
@@ -1,6 +1,40 @@
-var mailer = require('../lib/jademail')('../examples/example_mails');
+var mailer = require('../')('./examples/example_mails');
 var util = require('util');
 
+/** /
+mailer.setTransport({
+    host: 'mail.beonebee.it', // hostname
+    secureConnection: false, // DONT use SSL
+    port: 25, // port for IN-secure SMTP
+    auth: {
+        user: 'test@beonebee.it',
+        pass: 'b1b7792gh'
+    }
+});
+
+var emailHeaders = {
+  to: 'mister.gamer@gmail.com',
+  from: 'test@beonebee.it',
+  subject: 'forever troll'
+};
+
+
+mailer.on('profilesLoaded', function (path) {
+  mailer.profileCache['validation'].expiryDate = Date.now();
+  setTimeout(function(){
+    console.log(util.inspect(mailer.listProfiles(false), false, 3, true));
+    mailer.Send('validation', emailHeaders);
+  }, 10);
+  // console.log(util.inspect(mailer.listProfiles(false), false, 4, true));
+});
+
+mailer.on('profileAdded', function (profileName) {
+  console.log('profile \''+profileName+'\' added');
+});
+
+mailer.on('emailSent', function (to) {
+  console.log('email sent to \''+to);
+});
 
 /** /
 var transport = {
@@ -28,5 +62,53 @@ mailer1('spam', {transport: transport, email: email, to: to, from: from, subject
  console.log('mail...sent?');
 });
 
-/**/
+/** /
+var transport = nodemailer.createTransport('SMTP', {
+    host: 'mail.beonebee.it', // hostname
+    secureConnection: false, // DONT use SSL
+    port: 25, // port for IN-secure SMTP
+    auth: {
+        user: 'test@beonebee.it',
+        pass: 'b1b7792gh'
+    }
+});
 
+var text_email = fs.readFileSync('../views/email/test_email.txt', 'utf8');
+var jadetemplate = jade.compile(fs.readFileSync('../views/email/test_email.jade', 'utf8'));
+var css = fs.readFileSync('../public/css/email/style.css', 'utf8');
+var formatted_html = juice(jadetemplate(), css);
+// console.log(formatted_html);
+console.log('HTML Compiled!');
+
+
+var message = {
+  from: 'BeOneBee Test Account <test@beonebee.it',
+  to: 'mister.gamer@gmail.com',
+  subject: 'NodeMailer is Unicorn Friendly',
+  text: text_email,
+  html: formatted_html,
+  attachments: []
+};
+
+
+
+console.log('Sending Mail');
+transport.sendMail(message, function(error){
+    if(error){
+        console.log('Error occured');
+        console.log(error.message);
+        return;
+    }
+    console.log('Message sent successfully!');
+    
+    // if you don't want to use this transport object anymore, uncomment following line
+    transport.close(); // close the connection pool
+});
+
+
+
+
+
+exports.examplemsg = message;
+exports.transport = transport;
+/**/
